ece835aeca27b1abaf5e006587a37481c420466e
Samantha Hoy <samantha.hoy@gmail.com>
2016-07-04 15:10:22 +0200

    Server.js and /sales/week1 created using an html string parsed to the browser

--- product_stats.js
+++ product_stats.js
@@ -3,6 +3,13 @@ var mostProfitable = require('./most_profitable');
 var assert = require('assert');
 var Handlebars = require('handlebars');
 var fs = require('fs');
+var statsMap_week1 = {};
+
+// var express = require('express');
+// var handlebars = require('express-handlebars');
+// var bodyParser = require('body-parser');
+// var session = require('express-session');
+// var flash = require('express-flash');
 
 // it("it should return the most popular product"
 function getWeeklyValues(week) {
@@ -45,7 +52,8 @@ function getWeeklyValues(week) {
   // it("it should return the most profitable category"
   var products = allProducts.weeklySales(week); // gets all product data
   var productData = allProducts.groupedByProductName(products); // gets all product quantities
-  var totalSalesData = mostProfitable.groupedbyProductTotalSales(products); // get total product sales
+  var totalSalesData = mostProfitable.groupedby// var htmlStr = product_stats.getWeeklyStats(1);
+  // res.send(htmlStr);ProductTotalSales(products); // get total product sales
   var dateArray = mostProfitable.getPurchasesDateRange(week); // gets the dates
   var purchasesUnitCost = mostProfitable.getWeeklyPurchaseData(dateArray); // gets purchases unit cost
   var totalPurchasesData = mostProfitable.getPurchasePriceData(productData, purchasesUnitCost); // get total purchases sales
@@ -57,6 +65,9 @@ function getWeeklyValues(week) {
   var mostProfitableCategory = mostProfitable.getMostProfitable(catProdMap); // returns the most popular category
 
   var statsMap = { // shaping the data so that it can be used in handlebars
+    "title": "Nelisa's Spaza Shop",
+    "heading1": "Nelisa's Spaza Shop",
+    "heading2" : "Week " + week + "'s" + " Statistics",
     "mostPopularProduct": mostPopularProduct,
     "leastPopularProduct": leastPopularProduct,
     "mostPopularCategory": mostPopularCategory,
@@ -67,35 +78,39 @@ function getWeeklyValues(week) {
   return statsMap;
 }
 
-function createHtml(statsMap) {
+function createHtml(statsMap) { // creates the html string to be passed to my html page
   var source = fs.readFileSync('./views/week_template.hbs', 'utf8'); /* gets the template from the pathname */
   var template = Handlebars.compile(source); // handlebars template is compiled
   var data = statsMap; // the data being passed
   var result = template(data); // the compiled template with the data being passed
   return result;
 }
+// for (var i = 1; i < 5; i++) {  // creates a static html page for each week
+//   switch (i) {
+//     case 1:
+//       var statsMap_week1 = getWeeklyValues(i);
+//       var result = createHtml(statsMap_week1);
+//       var output_week1 = fs.writeFileSync("week1.html", result); // my html page is parsed to my filesync function and saved to disc as week1.html
+//       break;
+//     case 2:
+//       var statsMap_week2 = getWeeklyValues(i);
+//       var result = createHtml(statsMap_week2);
+//       var output_week2 = fs.writeFileSync("week2.html", result); // my html page is parsed to my filesync function and saved to disc as week2.html
+//       break;
+//     case 3:
+//       var statsMap_week3 = getWeeklyValues(i);
+//       var result = createHtml(statsMap_week3);
+//       var output_week3 = fs.writeFileSync("week3.html", result); // my html page is parsed to my filesync function and saved to disc as week3.html
+//       break;
+//     case 4:
+//       var statsMap_week4 = getWeeklyValues(i);
+//       var result = createHtml(statsMap_week4);
+//       var output_week4 = fs.writeFileSync("week4.html", result); // my html page is parsed to my filesync function and saved to disc as week4.html
+//       break;
+// }
 
-for (var i = 1; i < 5; i++) {  // creates a static html page for each week
-  switch (i) {
-    case 1:
-      var statsMap_week1 = getWeeklyValues(i);
-      var result = createHtml(statsMap_week1);
-      var output = fs.writeFileSync("week1.html", result);
-      break;
-    case 2:
-      var statsMap_week2 = getWeeklyValues(i);
-      var result = createHtml(statsMap_week2);
-      var output = fs.writeFileSync("week2.html", result);
-      break;
-    case 3:
-      var statsMap_week3 = getWeeklyValues(i);
-      var result = createHtml(statsMap_week3);
-      var output = fs.writeFileSync("week3.html", result);
-      break;
-    case 4:
-      var statsMap_week4 = getWeeklyValues(i);
-      var result = createHtml(statsMap_week4);
-      var output = fs.writeFileSync("week4.html", result);
-      break;
-  }
+exports.getWeeklyStats = function (week) {
+  var statsMap = getWeeklyValues(week);
+  var htmlString = createHtml(statsMap);
+  return htmlString;
 }
--- server.js
+++ server.js
@@ -2,14 +2,25 @@
 
 var express = require('express');
 var app = express();
-var main = require('./app.js');
+var product_stats = require('./product_stats.js');
 
 // create a route
-app.get('/hello', function(req, res) {
-  res.send('Hello Codex!');
+// app.get('/hello', function(req, res) {
+//   res.send('Hello Codex!');
+// });
+
+app.get('/sales/week1', function(req , res) { // this will send the output of my htmlstring to my browser
+  res.send(product_stats.getWeeklyStats(4));
+  // res.render("week_template",product_stats.getWeeklyStats(1));
 });
 
-//start the server
+// app.get('/sales/week/:week_no', function(req, res) { // cannot split the text and the variable part so a and extra / is necessary to be able to distinguish the variable part
+//   /// res.send("the week_num :" + req.params.week_no);
+//   var week_num = req.params.week_no;
+//   res.send(product_stats.getWeeklyStats(week_num));
+// });
+
+// start the server
 var server = app.listen(3000, function() {
   var host = server.address().address;
   var port = server.address().port;
