a07171928e3e875ceb9676543e4de1a70b66d044
Samantha Hoy <samantha.hoy@gmail.com>
2016-07-06 14:42:44 +0200

    modification to server.js and creation of package.json

--- server.js
+++ server.js
@@ -8,24 +8,30 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-app.get('/sales/week1', function(req , res) {
-  // var week_num = req.params.week_no ;
-  var data = product_stats.getWeeklyStats(1);
+app.get('/sales/week/:week_no', function(req , res) {
+  var week_num = Number(req.params.week_no) ;
+  var data = product_stats.getWeeklyStats(week_num);
   res.render('week_template', data );
 });
 
-app.get('/sales/week2', function(req , res) {
-  var data = product_stats.getWeeklyStats(2);
-  res.render('week_template', data );
-});
-app.get('/sales/week3', function(req , res) {
-  var data = product_stats.getWeeklyStats(3);
-  res.render('week_template', data );
-});
-app.get('/sales/week4', function(req , res) {
-  var data = product_stats.getWeeklyStats(4);
-  res.render('week_template', data );
-});
+// app.get('/sales/week1', function(req , res) {
+//   // var week_num = req.params.week_no ;
+//   var data = product_stats.getWeeklyStats(1);
+//   res.render('week_template', data );
+// });
+//
+// app.get('/sales/week2', function(req , res) {
+//   var data = product_stats.getWeeklyStats(2);
+//   res.render('week_template', data );
+// });
+// app.get('/sales/week3', function(req , res) {
+//   var data = product_stats.getWeeklyStats(3);
+//   res.render('week_template', data );
+// });
+// app.get('/sales/week4', function(req , res) {
+//   var data = product_stats.getWeeklyStats(4);
+//   res.render('week_template', data );
+// });
 
 // start the server
 var server = app.listen(3000, function() {
